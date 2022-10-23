db4b44c4fe8e74be5d2bb78d3943b81afb7034d1
Jhony00224 <biubio70@gmail.com>
2022-10-23 16:17:58 -0400

    Aula de front-end II

---
+++ calculadora.js
@@ -0,0 +1,10 @@
+import somar from "./somar";
+import divisao from "./dividir";
+import multiplicacao from "./multiplicar";
+import subtracao from "./subtrair";
+
+
+alert( multiplicacao(50, 80) );
+alert( somar(10, 20) );
+alert( subtracao(50, 80) );
+alert(divisao(0, 80))
